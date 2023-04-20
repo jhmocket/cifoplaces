1d7755fd3853c124c64f225a1b3b127f386ed3c5
jhmocket <mocket@gmail.com>
2023-04-20 19:36:32 +0200

    v0.1.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'robertsallent/fastlight',
+        'pretty_version' => 'v0.8.4',
+        'version' => '0.8.4.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'robertsallent/fastlight' => array(
+            'pretty_version' => 'v0.8.4',
+            'version' => '0.8.4.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
